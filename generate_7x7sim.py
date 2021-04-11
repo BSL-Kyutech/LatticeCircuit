8f90527d8f591d50ed65aaa1f5e2bc9be8b10815
Shuhei Ikemoto <shuhei.ikemoto@gmail.com>
2021-04-11 12:28:39 +0900

    Updated. All scripts are now executable properly.

--- generate_7x7sim.py
+++ generate_7x7sim.py
@@ -1,11 +1,19 @@
-import sys
+import argparse
 import numpy as np
 
+
 if __name__=='__main__':
-    args = sys.argv
-    M = int(args[1]) # M nodes between two nearest neighbor terminals
-    R = int(args[2]) # resistance (kOhm)
-    C = int(args[3]) # capacitance (nF)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("M", help="M edges between two input/output nodes (terminals)", type=int)
+    parser.add_argument("--R", help="resistance (kOhm)", default=1, type=int)
+    parser.add_argument("--C", help="capacitance (nF)", default=1, type=int)
+    parser.add_argument("--V", help="input voltage (V)", default=1, type=int)
+    args = parser.parse_args()
+
+    M = args.M
+    R = args.R
+    C = args.C
+    V = args.V
 
     N = (7+1)*M # N x N lattice
 
@@ -16,26 +24,48 @@ if __name__=='__main__':
         for j in range(7):
             m = (N+1)*M*(i+1)+M*(j+1)+1
             node_out.append(m)
+    node_out.remove(node_in)
 
     map_node = np.arange((N+1)*(N+1))
     map_node = map_node + 1
+    # translate to node labels and place output terminals
+    map_node = ["N%04d" % node for node in map_node]
+    for i in range(len(node_out)):
+        map_node = ["%d"%(i+1) if node==("N%04d" % node_out[i]) else node for node in map_node]
+    # reshape the list to a square matrix
+    map_node = np.array(map_node)
     map_node = map_node.reshape([(N+1),(N+1)])
+    
+    ##################################################
+    # Contents generation from here
+
+    # comment about setting
+    print( " * Size %d x %d (M=%d), R=%dkOhm, C=%dnF, V=%dV" % (N,N,M,R,C,V) )
 
     # rows of resisters
-#    count_r = 1
-#    count_terminal = 0
-#    for i in range(N+1):
-#        for j in range(N):
-#            print( "R%d N%d N%d %dk" % (count_r, i*N+(j+i+1), i*N+(j+i+1)+1, R) )
-#            count_r = count_r + 1
+    count_r = 1
+    for i in range(N+1):
+        for j in range(N):
+            print( "R%d %s %s %dk" % (count_r, map_node[i,j], map_node[i,j+1], R) )
+            count_r = count_r + 1
     # columns of resisters
-#    for i in range(N+1):
-#        for j in range(N):
-#            print( "R%d N%d N%d %dk" % (count_r, j*N+(j+i+1), j*N+(j+i+1)+1+N, R) )
-#            count_r = count_r + 1
-#    # capacitors
-#    count_r = 1
-#    for i in range((N+1)*(N+1)):
-#        print( "C%d N%d 0 %dn" % (count_r, i+1, C) )
-#        count_r = count_r + 1
+    for i in range(N):
+        for j in range(N+1):
+            print( "R%d %s %s %dk" % (count_r, map_node[i,j], map_node[i+1,j], R) )
+            count_r = count_r + 1
+    # capacitors
+    count_c = 1
+    for i in range(N+1):
+        for j in range(N+1):
+            print( "C%d %s 0 %dn" % (count_c, map_node[i,j], C) )
+            count_c = count_c + 1
     
+    # input
+    print( "V1 N%04d 0 AC %dV" % (node_in, V) )
+
+    # simulation setups
+    print( ".ac oct 1 10k 10k" )
+    for i in range(len(node_out)):
+        print( ".meas AC V(%d) FIND V(%d) AT 10k" % (i+1, i+1) )
+    print( ".backanno" )
+    print( ".end" )
--- suggest_setup.py
+++ suggest_setup.py
@@ -1,28 +1,36 @@
-import sys
+import argparse
 
 
 if __name__=='__main__':
-    args = sys.argv
-    M = int(args[1]) # N nodes between two nearest neighbor terminals
+    parser = argparse.ArgumentParser()
+    parser.add_argument("M", help="M edges between two input/output nodes (terminals)", type=int)
+    args = parser.parse_args()
+    M = args.M
     
     N = (7+1)*M
     center = (N+2)*(N/2)+1
     node_in = int(center)
     node_out = []
     for i in range(7):
-        print( "%d-th row" % (i+1) )
+        print( "%d-th row of terminals" % (i+1) )
         for j in range(7):
             m = (N+1)*M*(i+1)+M*(j+1)+1
             node_out.append(m)
             print(m)
+    node_out.remove(node_in)
     print("< Result >")
     print("Lattice size:")
     print("%d x %d lattice" % (N,N))
+    print("%d nodes in total" % ((N+1)*(N+1)))
     print("Input node:")
     print(node_in)
     print("Output nodes:")
     print(node_out)
     print("")
     print("< Example >")
-    print("python3 generate_net.py %d 1 1 > example.net" % N)
-    print("python3 insert_command.py")
+    print("You can generate the suggested grid RC circuit using 1kOhm resistance, 1nF capacitance, and 1V input as follows:")
+    print("")
+    print("python3 generate_net.py %d > example.net" % M)
+    print("")
+    print("For the detail, check the help \"python3 generate_net.py -h\"")
+    
