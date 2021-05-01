f4ee3eb96474ca30311fe5cb72e784c5881117cf
Shuhei Ikemoto <shuhei.ikemoto@gmail.com>
2021-05-01 17:14:31 +0900

    Add -g option to ground all outer edge nodes.

--- generate_base_netlist.py
+++ generate_base_netlist.py
@@ -14,6 +14,7 @@ if __name__=='__main__':
     parser.add_argument("--D", help="simulation duration (msec)", default=100, type=int)
     parser.add_argument('-s', '--sine', help='select SINE for the input', action='store_true')
     parser.add_argument('-p', '--pulse', help='select PULSE for the input', action='store_true')
+    parser.add_argument('-g', '--ground', help='set nodes on outer edges as GND', action='store_true')
     args = parser.parse_args()
 
     M = args.M
@@ -25,25 +26,56 @@ if __name__=='__main__':
     Ds = int(D*0.8)
     sine_flag = args.sine
     pulse_flag = args.pulse
+    gnd_flag = args.ground
 
-    N = (7+1)*M # N x N lattice
+    N = (7+1)*M # N+1 x N+1 lattice
 
     center = (N+2)*(N/2)+1
     node_in = int(center)
     node_out = []
+    node_edge = []
     for i in range(7):
         for j in range(7):
             m = (N+1)*M*(i+1)+M*(j+1)+1
             node_out.append(m)
     node_out.remove(node_in)
-
+    for i in range(N+1):
+        if (i+1) == 1:
+            for j in range(N+1):
+                node_edge.append(j+1)
+        elif (i+1) == N+1:
+            for j in range(N+1):
+                node_edge.append((i*(N+1))+j+1)
+        else:
+            node_edge.append((i*(N+1))+1)
+            node_edge.append((i*(N+1))+N+1)
+    
+    # make a map of all nodes
     map_node = np.arange((N+1)*(N+1))
     map_node = map_node + 1
-    # translate to node labels and place output terminals
+    # translate to node labels
+
+    # 全部入れ替えてから番号をそろえるようにしないとどんどんずれる！！！！
     map_node = ["N%03d" % node for node in map_node]
+
+    # place output terminals. if -g was given, place edge GND terminals as well.
     for i in range(len(node_out)):
         map_node = ["%d"%(i+1) if node==("N%03d" % node_out[i]) else node for node in map_node]
-        map_node = ["N%03d" % (int(re.sub(r'\D', '', node))-1) if int(re.sub(r'\D', '', node)) > node_out[i] else node for node in map_node]
+    if gnd_flag:
+        for i in range(len(node_edge)):
+            map_node = ["0" if node==("N%03d" % node_edge[i]) else node for node in map_node]
+    # make a list of replaced nodes
+    node_replaced = node_out
+    if gnd_flag:
+        node_replaced.extend(node_edge)
+    node_replaced.sort(reverse=True)
+    # fill gaps of node numbers by replaced nodes (make node numbers serial)
+    for i in range(len(node_replaced)):
+        map_node = ["N%03d" % (int(re.sub(r'N([0-9]+)', r'\1', node))-1) 
+        if int(re.sub(r'N([0-9]+)', r'\1', node)) > node_replaced[i] and len(re.sub(r'N([0-9]+)', r'\1', node)) > 2 
+        else node 
+        for node in map_node]
+    
     # reshape the list to a square matrix
     map_node = np.array(map_node)
     map_node = map_node.reshape([(N+1),(N+1)])
@@ -58,19 +90,28 @@ if __name__=='__main__':
     count_r = 1
     for i in range(N+1):
         for j in range(N):
-            print( "R%d %s %s %dk" % (count_r, map_node[i,j], map_node[i,j+1], R) )
-            count_r = count_r + 1
+            if map_node[i,j] == "0" and map_node[i,j+1] == "0":
+                pass
+            else:
+                print( "R%d %s %s %dk" % (count_r, map_node[i,j], map_node[i,j+1], R) )
+                count_r = count_r + 1
     # columns of resisters
     for i in range(N):
         for j in range(N+1):
-            print( "R%d %s %s %dk" % (count_r, map_node[i,j], map_node[i+1,j], R) )
-            count_r = count_r + 1
+            if map_node[i,j] == "0" and map_node[i+1,j] == "0":
+                pass
+            else:
+                print( "R%d %s %s %dk" % (count_r, map_node[i,j], map_node[i+1,j], R) )
+                count_r = count_r + 1
     # capacitors
     count_c = 1
     for i in range(N+1):
         for j in range(N+1):
-            print( "C%d %s 0 %dn" % (count_c, map_node[i,j], C) )
-            count_c = count_c + 1
+            if map_node[i,j] == "0":
+                pass
+            else:
+                print( "C%d %s 0 %dn" % (count_c, map_node[i,j], C) )
+                count_c = count_c + 1
     
     # input
     #print( "V1 N%03d 0 AC %dV" % (node_in, V) )
